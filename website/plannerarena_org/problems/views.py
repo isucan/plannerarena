43c7f5bfadcd03265e01bb119a7c7c8071405400
Ioan Sucan <isucan@willowgarage.com>
2011-12-20 19:03:36 +0200

    generating some tables

--- website/plannerarena_org/problems/views.py
+++ website/plannerarena_org/problems/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.safestring import mark_safe
+import sys
+import os
 
-def hw():
-    return "Hello, world. You're at the poll index."
+try:
+    import ompl_benchmark
+except:
+    sys.path.append(os.environ['PLANNERARENA_ROOT'] + "/scripts/ompl-benchmark")
+    import ompl_benchmark
+    
+benchmark = ompl_benchmark.OMPLBenchmark(os.environ['PLANNERARENA_ROOT'] + "/db/benchmark.db")
 
 def index(request):
-    return render_to_response('problems/index.html', {'request' : request, 'page_content': hw() }, 
+    return render_to_response('problems/index.html', {'request' : request, 'page_content': "test" }, 
+                              context_instance=RequestContext(request))
+
+def list_experiments(request, exps):
+    content = "pg"
+    return render_to_response('problems/index.html', {'request' : request, 'page_content': mark_safe(content) }, 
                               context_instance=RequestContext(request))
 
 def subindex_geometric(request):
-    return render_to_response('problems/index.html', {'request' : request, 'page_content': 'sub page!' }, 
-                              context_instance=RequestContext(request))
+    return list_experiments(request, benchmark.getGeometricExperimentNames())
 
 def subindex_control(request):
-    return render_to_response('problems/index.html', {'request' : request, 'page_content': 'sub page!' }, 
-                              context_instance=RequestContext(request))
+    return list_experiments(request, benchmark.getControlExperimentNames())
--- website/plannerarena_org/results/views.py
+++ website/plannerarena_org/results/views.py
@@ -1,13 +1,83 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.safestring import mark_safe
+import sys
+import os
+
+try:
+    import gviz_api
+except:
+    sys.path.append(os.environ['PLANNERARENA_ROOT'] + "/scripts/google-visualization-python")
+    import gviz_api
+
+try:
+    import ompl_benchmark
+except:
+    sys.path.append(os.environ['PLANNERARENA_ROOT'] + "/scripts/ompl-benchmark")
+    import ompl_benchmark
+
+benchmark = ompl_benchmark.OMPLBenchmark(os.environ['PLANNERARENA_ROOT'] + "/db/benchmark.db")
+
+def toJson(tbl):
+    data_table = gviz_api.DataTable(tbl.description)
+    data_table.LoadData(tbl.data)
+    return data_table.ToJSon(columns_order=[c[0] for c in tbl.description], order_by=tbl.orderby)
 
 def hw():
-    return "Hello, world. You're at the poll index."
+    x = " "#.join(getattr(problems, benchmark).getExperimentNames())
+    return "Hello, world. You're at the poll index." + x
+
+def getJsTables(tables):
+    table_template = """
+<script src="https://www.google.com/jsapi" type="text/javascript"></script>
+<script>
+    google.load('visualization', '1', {packages:['table', 'corechart', 'motionchart']});
+    google.setOnLoadCallback(drawTable);
+    function drawTable() {
+      %s
+    }
+  </script>
+"""
+    js = ""
+    div = ""
+    for t in tables:
+        js += t[0]
+        div += t[1]
+    return (table_template % js, div)
+    
+def getJsTable(name, json, chart = True):
+    from string import replace
+    tname = replace(name, " ", "_")
+    js = "var json_data_%s = new google.visualization.DataTable(%s, 0.6);\n\n" % (tname, json)
+    if chart:
+        js = js + "var json_chart_%s = new google.visualization.MotionChart(document.getElementById('chart_div_%s'));\n" % (tname, tname)
+        js = js + "var chart_%s_options = {};\n" % tname
+        js = js + "chart_%s_options['state'] = '{\"playDuration\":1500}';\n" % tname
+        js = js + "chart_%s_options['width'] = 640;\n" % tname
+        js = js + "chart_%s_options['height'] = 420;\n" % tname
+        js = js + "json_chart_%s.draw(json_data_%s, chart_%s_options);\n" % (tname, tname, tname)
+    else:
+        js = js + "var json_table_%s = new google.visualization.Table(document.getElementById('table_div_%s'));\n" % (tname, tname)
+        js = js + "json_table_%s.draw(json_data_%s, {showRowNumber: false});\n" % (tname, tname)
+    div = ""
+    if chart:
+        div = '<h2>%s</h2><div id="chart_div_%s"></div>' % (name, tname)
+    else:
+        div = '<h2>%s</h2><div id="table_div_%s"></div>' % (name, tname)
+    return [js, div]
 
 def index(request):
-    return render_to_response('results/index.html', {'request' : request, 'page_content': hw() }, 
+    tables = []
+    tables.append(getJsTable("all_exps", toJson(benchmark.getExperimentsTable()), False))
+    exps = benchmark.getGeometricExperimentNames()
+    for e in exps:
+        tables.append(getJsTable(e, toJson(benchmark.getGeometricPlannersTable(e)), True))
+
+    (js, div) = getJsTables(tables)
+    content = js + div
+    return render_to_response('results/index.html', {'request' : request, 'page_content': mark_safe(content) }, 
                               context_instance=RequestContext(request))
 
-def subindex(request):
-    return render_to_response('results/index.html', {'request' : request, 'page_content': 'sub page!' }, 
-                              context_instance=RequestContext(request))
+#def subindex(request):
+#    return render_to_response('results/index.html', {'request' : request, 'page_content': 'sub page!' }, 
+#                              context_instance=RequestContext(request))
